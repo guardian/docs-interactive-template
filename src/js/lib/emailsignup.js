6b10454ee0906f430cdefa04888f25107867fcb4
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-08-25 15:31:51 +0100

    wip add post

--- src/js/lib/emailsignup.js
+++ src/js/lib/emailsignup.js
@@ -5,7 +5,28 @@ import formSerialize from 'form-serialize'
 export default function emailsignup(formNode) {
     formNode.addEventListener("submit", function(event){
         event.preventDefault();
-        console.log(formSerialize(formNode));
+        var formParams = formSerialize(formNode);
+
+        reqwest({
+            url: 'https://api.nextgen.guardianapps.co.uk/email'
+            , method: 'post'
+            , type: 'json'
+            , data: formParams
+            , crossOrigin: true
+            , complete: function (resp) {
+                console.log(resp);
+                if(resp.status === 201){
+                    console.log("SUCCESS")
+                }
+                else {
+                    console.log("FAILURE");
+                }
+            }
+            // eslint-disable-next-line
+            , error: function (err) {
+                console.warn(err)
+            }
+        });
     });
 
 }
