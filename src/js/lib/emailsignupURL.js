d52f38476a871ed4b02bcfc95e2e8d59173fb086
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-02-10 13:57:10 +0000

    fix lint

--- src/js/lib/emailsignupURL.js
+++ src/js/lib/emailsignupURL.js
@@ -1,4 +1,4 @@
 export default function emailsignupURL(listId) {
-    const isApp = window.location.protocol === "file:";
+    const isApp = window.location.protocol === 'file:';
     return window.location.origin === 'https://www.theguardian.com' || isApp ? `https://www.theguardian.com/email/form/plaindark/${listId}` : `https://m.code.dev-theguardian.com/email/form/plaindark/${listId}`;
 }
