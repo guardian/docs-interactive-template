ca646db499e4783c76b20a68859b1333fd4e1629
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-04-24 18:37:50 +0200

    remvoe some logs

--- src/js/lib/sheettodom.js
+++ src/js/lib/sheettodom.js
@@ -2,9 +2,7 @@ import startsWith from 'lodash.startswith';
 
 export function sheetToDomInnerHtml({ el, docData, comingSoonSheetName }) {
     Array.from(el.querySelectorAll('[data-sheet-attribute]')).forEach(node => {
-        console.log('node', node);
         const field = node.getAttribute('data-sheet-attribute');
-        console.log('field', field);
 
         if (startsWith(field, `${comingSoonSheetName}-`) && docData.comingNext) {
             node.innerHTML = docData.comingNext[field.split(`${comingSoonSheetName}-`)[1]];
