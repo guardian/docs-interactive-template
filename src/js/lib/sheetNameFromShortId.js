7eb63fc8ac3464e0de014167d5912975a209214c
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-05-08 11:12:27 +0200

    add quarantine-irianian-style as default

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'scars';
+const DEFAULT_DOC = 'quarantine-irianian-style';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
