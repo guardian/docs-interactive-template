e229f41aa3ffa86abc52371ce257af47b6a524aa
Harry Fischer <harry.fischer@guardian.co.uk>
2020-12-09 12:11:29 +0000

    adding wolf of norway to bootjs

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'colette';
+const DEFAULT_DOC = 'the-wolf-dividing-norway';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
