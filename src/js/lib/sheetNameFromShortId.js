4d80b6bea2c63589f523276ed4718edd09157e19
Harry Fischer <harry.fischer@guardian.co.uk>
2021-02-18 16:39:50 +0000

    adding My Brothers Keeper

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'untold-chaos';
+const DEFAULT_DOC = 'my-brothers-keeper';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
