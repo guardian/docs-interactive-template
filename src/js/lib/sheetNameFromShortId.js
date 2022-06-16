6ab3702043ba429932cd5e72dfa6605ef008f5a7
Catriona Morrison <catriona.morrison@guardian.co.uk>
2022-06-16 11:41:35 +0100

    update

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'sending-aya-back';
+const DEFAULT_DOC = 'beirut-dreams-in-color';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
