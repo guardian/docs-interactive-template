6ee48ef34e070afa218ef4ce3735eaa15dd0b72e
Harry Fischer <harry.fischer@guardian.co.uk>
2022-07-19 13:45:37 +0100

    new doc

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'beirut-dreams-in-color';
+const DEFAULT_DOC = 'dear-bradford';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
